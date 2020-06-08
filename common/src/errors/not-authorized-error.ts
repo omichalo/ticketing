752c899981f840d8e6e8ed409a8ce4f634db4b9c
Omichalo <9549613+omichalo@users.noreply.github.com>
2020-06-08 15:23:35 +0200

    remove common submodule

---
+++ common/src/errors/not-authorized-error.ts
@@ -0,0 +1,21 @@
+import { CustomError } from './custom-error';
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 401;
+  reason = 'Not Authorized';
+
+  constructor() {
+    super('Not Authorized');
+
+    // Only because we are extending a built in class
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }
+}
